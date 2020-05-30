51357ae9e0f0db54c61111c3af132f3c82d6c826
Misha Gringauze <mgringauze@gmail.com>
2020-05-29 21:20:21 -0700

    added delete route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const db = require("../models");
 
 module.exports = app => {
+    app.delete("/:id", (req, res) => {
+        db.Book
+            .findByIdAndDelete(req.params.id)
+            .then(data => res.json(data))
+            .catch(err => console.log(err));
+    });
+
     app.get("/", (req, res) => {
         db.Book
             .find({})
